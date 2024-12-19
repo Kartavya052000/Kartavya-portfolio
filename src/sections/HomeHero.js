2b6bb7a7e51e487fc7b87c346e71b00b23143032
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-12-19 02:12:00 -0800

    update portfolio profile pic

--- src/sections/HomeHero.js
+++ src/sections/HomeHero.js
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { Typewriter, Cursor } from 'react-simple-typewriter';
 // components
 import SocialLinks from '../components/social/SocialLinks';
-import mainImg from '../assets/images/my_pic.png';
+import mainImg from '../assets/images/Kartavya.jpg';
 // other
 import { aboutParagraph, HERO_TITLES } from '../mock/profile';
 import { ScrollContext } from '../context/ScrollContext';
@@ -34,8 +34,8 @@ export default function HomeHero() {
           }}
           viewport={{ once: true }}
         >
-          <Image src={mainImg} className="w-52 object-contain lg:w-64" alt="Logo" />
-        </motion.div>
+<Image src={mainImg} className="w-52 object-contain lg:w-64 rounded-full" alt="Logo" />
+</motion.div>
         <h1 className="z-50 font-lato text-2xl font-bold sm:text-4xl md:font-semibold">
           Hi, I'm{' '}
           <span className="text-primary-700 dark:text-primary-300">
